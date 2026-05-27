1c51e614a2a8a42107483ae69d5555fe2350e321
Günter Grodotzki <gunter@grodotzki.com>
2026-05-27 02:09:30 +0200

    Fix ecs updater (#6)

--- Formula/exec-ecs.rb
+++ Formula/exec-ecs.rb
@@ -1,26 +1,26 @@
 class ExecEcs < Formula
   desc "Interactive ECS execute-command helper"
   homepage "https://github.com/DigitalTolk/exec-ecs"
-  version "1.1.9"
+  version "1.2.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/DigitalTolk/exec-ecs/releases/download/v#{version}/exec-ecs_Darwin_arm64.tar.gz"
-      sha256 "82ec4966780ee11f8fb2a53102fbe493f9ef04fbb1be870e9e886b7bc2e0b165"
+      sha256 "db74c63a427949c5b993d0b818418e2e36e89eff4bb7e2bec7d007039d6cc903"
     else
       url "https://github.com/DigitalTolk/exec-ecs/releases/download/v#{version}/exec-ecs_Darwin_x86_64.tar.gz"
-      sha256 "ac54757ddfe321dda4ef3a8b2cfb17cb66dbc36e71fd21e40cdfb0955dd090f4"
+      sha256 "96f6136f39d2b01b74b42fcb196c67456ab9b2308324f34e3f7433abf431be52"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/DigitalTolk/exec-ecs/releases/download/v#{version}/exec-ecs_Linux_arm64.tar.gz"
-      sha256 "fe48d260ab3a9874596266adfe06ae37f99fc055fc1588af7ff703fafa9b619d"
+      sha256 "145d06bfb833f9911335faf13ba8d8acf2dd42d1ce71791f6752e6903a1b28d4"
     else
       url "https://github.com/DigitalTolk/exec-ecs/releases/download/v#{version}/exec-ecs_Linux_x86_64.tar.gz"
-      sha256 "3a398f532912035a0e1b7cef2045babf283356b7faabebc80738ca3744168978"
+      sha256 "8c4fca3075f6b8d03553738bae679da382988ffe6e5f39ea8cee1bb1cb3cf85d"
     end
   end
 
