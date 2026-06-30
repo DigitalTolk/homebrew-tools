30e6d5e6d37b34b12137e9537bb93de1fc73e784
lifeofguenter <241073+lifeofguenter@users.noreply.github.com>
2026-06-30 21:23:54 +0000

    Update keel formula

--- Formula/keel.rb
+++ Formula/keel.rb
@@ -1,26 +1,26 @@
 class Keel < Formula
   desc "First-step server bootstrap and ops tool"
   homepage "https://github.com/DigitalTolk/keel"
-  version "0.1.3"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/DigitalTolk/keel/releases/download/v#{version}/keel_Darwin_arm64.tar.gz"
-      sha256 "3630d16053024292fee0523c938aa4319ea11e6d5c320b9e1ff18101d31f6102"
+      sha256 "bd7a69a1b6a2d56269289317683bda659c1e4dfe2ec7f64c87eacff2fb39001f"
     else
       url "https://github.com/DigitalTolk/keel/releases/download/v#{version}/keel_Darwin_x86_64.tar.gz"
-      sha256 "b0926f385e44cc447f4870f3c8a6743ab5e9b7d93b04363f0f35e8bd6891717c"
+      sha256 "88be86f34b0eece1c2231d4a0b766a5a8314c378da883c21b362b63c379abbde"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/DigitalTolk/keel/releases/download/v#{version}/keel_Linux_arm64.tar.gz"
-      sha256 "c8cd0d9158b59fdabc3051cbee8bfae24e5d33c4e7b8d9cd622d0d44db73d8e1"
+      sha256 "0e3fd3ede8654fc730d7938a312b16993c56b5d5e6cb2057f1150a6f91c7a564"
     else
       url "https://github.com/DigitalTolk/keel/releases/download/v#{version}/keel_Linux_x86_64.tar.gz"
-      sha256 "885eefea3e77856f722cb5d743e6e9ea82fd3f5ae6bd84cb13facc602dc80878"
+      sha256 "184d3ab7fb88377805b18a5cdae3fd8a25c5563bc615ad7e50ee17fc18e5a8d4"
     end
   end
 
